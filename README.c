67d5efbad85c00f0861b4d482bf44f39c8757ea3
siddhanthwankhade <114637494+siddhanthwankhade@users.noreply.github.com>
2022-10-06 22:29:28 -0700

    Update README.c

--- README.c
+++ README.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main(){
     int a[3][3],i,j,k,s[2],min,max;
+    printf("enter the matrix");
     for(i=0;i<3;i++){
         for(j=0;j<3;j++){
             scanf("%d",&a[i][j]);
